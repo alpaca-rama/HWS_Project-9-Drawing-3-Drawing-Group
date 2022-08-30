a96803d052f3a26dfc5a25da0f53fca4bf19811e
Luca <luca@weve-got-it.com>
2022-08-30 12:25:41 +0200

    Added Content: Lesson -> Enabling high-performance Metal rendering with drawingGroup()

--- HWS_Project-9-Drawing-3-Drawing-Group/ContentView.swift
+++ HWS_Project-9-Drawing-3-Drawing-Group/ContentView.swift
@@ -7,10 +7,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ColourCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5),
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                        )
+            }
+        }
+        // This modifier greatly improves the performance of the render.
+        // try remove it and see the difference
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct ContentView: View {
+    @State private var colorCycle = 0.0
+    
+    var body: some View {
+        VStack {
+            ColourCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $colorCycle)
+        }
     }
 }
 
